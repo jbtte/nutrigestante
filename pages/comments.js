cca6d44a22938c1dc3e6cddbec09aff10beebac9
Joao Baumotte <jbaumotte@gmail.com>
2021-05-27 11:24:42 -0300

    Build Loading spinner Comments Page

--- pages/comments.js
+++ pages/comments.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useForm } from "react-hook-form";
 import { Content, Bar, FormDiv, ImageDiv, SavedDiv} from '../components/styled/CommentsPageStyled'
 
@@ -7,20 +7,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Theme from "../theme/main";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Spinner from 'react-bootstrap/Spinner'
 import Image from 'next/image'
 
 
 export default function Comments() {
   const [saved, setSaved ] = useState(false)
+  const [loading, setLoading] = useState(false)
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  
+    
   const onSubmit = async ({name, message}, e) => {
+    setLoading(true)
+    
     try {
       await fetch (`/api/comments?name=${name}&message=${message}`, {method: 'POST'})
       e.target.reset()
       setSaved(true)
+
     } catch (e) {
       console.error(e)
+
+    } finally {
+      setLoading(false)
+
     }
   };
 
@@ -57,7 +66,11 @@ export default function Comments() {
                   />
                  {errors.message && <span>Conte alguma coisa pra mim</span>}
               </Form.Group>
-              <Button className="btn-pink" type="submit"> Enviar </Button>
+              <Button className="btn-pink" type="submit" disabled={loading} > 
+              {loading ?  
+              <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true"/> :
+              "Enviar" }
+              </Button>
               <SavedDiv>
                 {saved && <h5>Sua Mensagem foi salva</h5>}
               </SavedDiv>
