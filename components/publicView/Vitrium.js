165464320a47416006947ffa4daca82c93d8f8c2
JP Baumotte <jbaumotte@gmail.com>
2024-01-03 12:57:57 -0300

    Update address - Vitrium.js

--- components/publicView/Vitrium.js
+++ components/publicView/Vitrium.js
@@ -6,8 +6,8 @@ export default function Vitrium () {
       <CrossBar className="d-flex jsutify-content-between">
         <div>
           <p>Onde eu atendo</p>
-          <h4>Vitrium Centro Médico </h4>
-          <p>SGAS 614 - L2 Sul - Sala 151  - (<a href="https://www.google.com.br/maps/place/Nutricionista+para+Gestantes+-+Nutri%C3%A7%C3%A3o+gestante/@-15.8357103,-47.9142477,18z/data=!4m5!3m4!1s0x0:0x214df389edb21b55!8m2!3d-15.8360827!4d-47.91323" target="_blank">Google maps</a>)</p>
+          <h4>Terraço Shopping </h4>
+          <p>Octogonal Terraço Shopping Torre A sala 104 (<a href="https://www.google.com.br/maps/place/Nutrigestante+-+Fab%C3%ADola+Amaral/@-15.8037721,-47.9428632,17z/data=!3m1!4b1!4m6!3m5!1s0x935a255adc99935b:0x214df389edb21b55!8m2!3d-15.8037773!4d-47.9402829!16s%2Fg%2F11h03zvk7b?entry=ttu" target="_blank">Google maps</a>)</p>
         </div>
       </CrossBar>
     </Content>
